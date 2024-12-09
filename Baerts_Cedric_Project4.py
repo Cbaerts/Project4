fe01eea442c21f2a2856d5f958c61d0bb7748988
Cbaerts <cbaerts@uwo.ca>
2024-12-09 14:06:26 -0500

    Update Baerts_Cedric_Project4.py
    Comments

--- Baerts_Cedric_Project4.py
+++ Baerts_Cedric_Project4.py
@@ -25,13 +25,12 @@ def make_tridiagonal(N, b, d, a):
     matrix: ndarray
         The tridiagonal matrix based on the inputted values
     '''
-
     matrix = np.zeros((N, N))
     aMatrix, bMatrix, dMatrix = a*np.eye(N, k=1), b*np.eye(N, k=-1), d*np.eye(N, k=0)
     matrix = matrix + aMatrix + bMatrix + dMatrix
 
     matrix[0, -1] = b
-    matrix[-1, 0]  = a
+    matrix[-1, 0] = a
     return matrix
 
 def make_initialcond(wParam, xArray):
@@ -39,14 +38,20 @@ def make_initialcond(wParam, xArray):
     Takes a position array as input and returns the initial condition for the positions in the input array.
     The initial condition is a gaussian function times a cosine function.
 
-    Args:
-    sigma0 (float): Standard Deviation of the gaussian function.
-    k0 (float): Average wave number
-    xArray (numpy array): Position grid
-    x0 (float): 
+    Parameters
+    ----------
+    wParam: nd.array
+        [sigma0, x0, k0], where
+        sigma0: Standard Deviation of the gaussian function.
+        k0: Average wave number
+        x0: Central position 
+    xArray: nd.array 
+        Position grid
 
-    Returns:
-    a0 (Numpy Array): Initial condition corresponding to the input position array.
+    Returns
+    ---------
+    a0: nd.array 
+        Initial condition for psi for the schrodinger equation.
     """
     sigma0, x0, k0 = wParam
     psi = np.exp(i*k0*xArray)*np.exp(-(xArray-x0)**2/(2*sigma0**2))/(np.sqrt(sigma0*np.sqrt(np.pi)))    
@@ -71,53 +76,101 @@ def spectral_radius(matrix):
 
 def sch_eqn(nspace, ntime, tau, method, length, wparam = [10, 0, 0.5]):
     '''
-    nspace is the number of spatial grid points, 
-    ntime is the number of time steps to be evolved,
-    tau is the time step to be used
-    method: string, either ftcs or crank
-    length: float, size of spatial grid. Default to 200 (grid extends from -100 to +100)
-    wparam: list of parameters for initial condition [sigma0, x0, k0]. Default [10, 0, 0.5].
-    '''
-    tArray = np.arange(0, ntime*tau, tau)
-    wavePacket = make_initialcond(wparam, xArray)
-    psi = np.zeros((ntime, nspace), dtype=complex)
-    psi[0, :] = wavePacket
-    h = length/(nspace-1)
-    I = np.eye(nspace)
-    constant = -hBar**2/(2*mass*h**2)
+    Parameters
+    ---------
+    nspacet: int
+        The number of spatial grid points, 
+    ntime: int
+        The number of time steps to be evolved,
+    tau: int
+        The time step to be used
+    method: str
+        Either ftcs or crank-nicholson
+    length: float 
+        size of spatial grid. Default to 200 (grid extends from -100 to +100)
+    wparam: nd.array
+        List of parameters for initial condition [sigma0, x0, k0]. Default [10, 0, 0.5].
 
+    Return
+    ---------
+    psi: nd.array
+        [ntime x nspace] shaped array which show the time evolution of psi
+    xArray: nd.array
+        1-D Array of all the points taken for psi
+    tArray: nd.array
+        1-D Array for all the time steps
+    prob: nd.array
+        [ntime x nspace] Array for the probabilities at all points and timesteps
+    '''
+    # Sets time stamps
+    tArray = np.arange(0, ntime*tau, tau)
+    # Create intial wavepacket
+    wavePacket = make_initialcond(wparam, xArray)
+    # Create empty complex array 
+    psi = np.zeros((ntime, nspace), dtype=complex)
+    # Set first line to the initial wavepacket
+    psi[0, :] = wavePacket
+    # Step distance
+    h = length/(nspace-1)
+    # Identity matrix
+    I = np.eye(nspace)
+    # Constant multiplied by both mehtods
+    constant = -hBar**2/(2*mass*h**2)
+    # Checks Method for FTCS
     if method.lower() in ['ftcs', '1']:
-        # eqn 9.31
+        # Makes Eqn 9.31 in Textbook
         H = make_tridiagonal(nspace, constant, 1-2*constant, constant)
         H = H * (i*tau/hBar)
-        eig = spectral_radius(H) 
-
+        # Checks if the solution will be stable
+        eig = spectral_radius(I-H) 
+        # If solution is found to be unstable it will stop integration.
         if eig > 1:
             return 'The solution will be unstable. \n I WILL NOT INTEGRATE'
+        # If solution is stable intergation will begin
         else: 
             print('The solution will be stable')
             for n in range(ntime-1):
+                # Equation 9.32 in textbook
                 psi[n+1, :] = np.dot((I-H), psi[n, :])
-
+    # Checks Method for CRANK
     elif method.lower() in ['crank', 'crank-nicholson', '2']:
-        # 9.46
+        # Equation 9.46 in the Textbook
         H = make_tridiagonal(nspace, constant, -2*constant, constant)
-        # 9.40
+        # Equation 9.40 in the Textbook
         HN = np.dot(np.linalg.inv(I + (i*tau/2/hBar)*H), I - (i*tau/2/hBar)*H)
         for n in range(1, ntime):
-            psi[n, :] = np.dot(HN, psi[n-1, :])       
+            # Iterates through 
+            psi[n, :] = np.dot(HN, psi[n-1, :])    
+    # Returns a statement if no actual method was selected  
     else:
         return 'Choose an actual method!'
-
+    # Calculates probability
+    # Under Equation 9.42 in the Textbook
     prob = np.abs(psi*np.conjugate(psi))
     return psi, xArray, tArray, prob
 
 def sch_plot(solution, figure):
+    '''
+    Parameters
+    ---------
+    Solution: tuple
+        All the returns from the sch_eqn function. 
+        [psi, xArray, tArray, prob]
+    figure: list
+        2 Item list which states if you want to get a psi plot and probablility plot
+    Return
+    ---------
+    '''
+    # Set solutions to their respective arrays
     psi, xArray, tArray, prob = solution
+    # Set a number so always 5 timesteps are shown
+    num = len(psi)/5
+    # Checks if psi plot wants to be created
+    # Plots psi
     if figure[0].upper() == 'Y':
         plt.subplot()
         for i,T in enumerate(tArray):
-                if i%100 == 0:
+                if i % num == 0:
                     plt.plot(xArray, np.real(psi[i]), label=f"Time step= {i}")
     plt.title("Wave Function of the Schrodinger Equation")
     plt.legend()
@@ -126,9 +179,11 @@ def sch_plot(solution, figure):
     # plt.savefig("BaertsCedric_fig1_Project4")
     plt.show()
     plt.close()
+    # Checks if probability plot wants to be created
+    # Plots probability
     if figure[1].upper() == 'Y':
         for i, T in enumerate(tArray):
-                if i%100 == 0:
+                if i % num == 0:
                     plt.plot(xArray, np.real(prob[i]), label=f"Time step = {i}")
     plt.title("Probabilty of The Schrodinger Equation")
     plt.legend()
@@ -153,15 +208,17 @@ tau = float(input("Enter a value for tau: "))
 
 # Initiates position grid.
 xArray = np.linspace(-length / 2, length / 2, nspace)
-# Initial parameters
+# Initial wave packet parameters
 parameters = [10, 0, 0.5]
 
 # Call your function with the provided parameters
 solution = sch_eqn(nspace, ntime, tau, method, length, parameters)
 
+# Checks if your solution returned an error or was right
 if isinstance(solution, str):
     print(solution)
 else:
-    figure = [input("Do you want psi plot (Y/N): "), input("Do you want prob plot (Y/N): ")]
+# If an actual solution asks if you want to see the Psi and/or probability plot
+    figure = [input("Do you want psi plot (Y/N): "), input("Do you want probability plot (Y/N): ")]
     plot = sch_plot(solution, figure)
 
